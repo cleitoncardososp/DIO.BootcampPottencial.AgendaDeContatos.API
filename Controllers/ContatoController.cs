2a48430d74d7ff1d53c519969754e0e6659b558c
Cleiton <cleitoncardososp@gmail.com>
2022-10-11 00:56:43 -0300

    update: tratamento trycatch

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -23,8 +23,6 @@ namespace Dio.AgendaDeContatos.Controllers
         {
             try
             {
-                throw new Exception();
-
                 _context.Contatos.Add(contato);
                 _context.SaveChanges();
                 return CreatedAtAction(nameof(GetById), new { id = contato.Id }, contato);
@@ -44,65 +42,130 @@ namespace Dio.AgendaDeContatos.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            List<Contato> listaDeContatos = _context.Contatos.ToList();
+            try
+            {
+                List<Contato> listaDeContatos = _context.Contatos.ToList();
 
-            if (listaDeContatos == null)
-                return NotFound();
+                if (listaDeContatos == null)
+                    return NotFound();
 
-            return Ok(listaDeContatos);
+                return Ok(listaDeContatos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception: " + ex + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Title = "Erro",
+                    Msg = $"Erro ao listar registros!",
+                    Status = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetById(int id)
         {
-            Contato contato = _context.Contatos.Find(id);
+            try
+            {
+                Contato contato = _context.Contatos.Find(id);
 
-            if (contato == null)
-                return NotFound();
+                if (contato == null)
+                    return NotFound();
 
-            return Ok(contato);
+                return Ok(contato);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception: " + ex + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Title = "Erro",
+                    Msg = $"Erro ao localizar registro!",
+                    Status = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
         [HttpGet("ObterPorNome/{nome}")]
         public IActionResult BuscaPorNome([FromRoute] string nome)
         {
-            var listaDeContatos = _context.Contatos.Where(c => c.Nome.Contains(nome));
+            try
+            {
+                var listaDeContatos = _context.Contatos.Where(c => c.Nome.Contains(nome));
 
-            if (listaDeContatos == null)
-                return NoContent();
+                if (listaDeContatos == null)
+                    return NoContent();
 
-            return Ok(listaDeContatos);
+                return Ok(listaDeContatos);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception: " + ex + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Title = "Erro",
+                    Msg = $"Erro ao localizar registro!",
+                    Status = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
-        [HttpPut("atualizar/{id:int}")]
+        [HttpPut("Atualizar/{id:int}")]
         public IActionResult Atualizar([FromRoute] int id, [FromBody] Contato contato)
         {
-            Contato contatoToUpdate = _context.Contatos.Find(id);
+            try
+            {
+                Contato contatoToUpdate = _context.Contatos.Find(id);
 
-            if (contatoToUpdate == null)
-                return NotFound();
+                if (contatoToUpdate == null)
+                    return NotFound();
 
-            contatoToUpdate.Nome = contato.Nome;
-            contatoToUpdate.Telefone = contato.Telefone;
-            contatoToUpdate.Ativo = contato.Ativo;
+                contatoToUpdate.Nome = contato.Nome;
+                contatoToUpdate.Telefone = contato.Telefone;
+                contatoToUpdate.Ativo = contato.Ativo;
 
-            _context.SaveChanges();
+                _context.SaveChanges();
 
-            return Ok(contatoToUpdate);
+                return Ok(contatoToUpdate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception: " + ex + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Title = "Erro",
+                    Msg = $"Erro ao atualizar registro!",
+                    Status = HttpStatusCode.InternalServerError
+                });
+            }
         }
 
-        [HttpDelete("deletar/{id:int}")]
+        [HttpDelete("Deletar/{id:int}")]
         public IActionResult Deletar([FromRoute] int id)
         {
-            Contato contato = _context.Contatos.Find(id);
+            try
+            {
+                Contato contato = _context.Contatos.Find(id);
 
-            if (contato == null)
-                return NotFound();
+                if (contato == null)
+                    return NotFound();
 
-            _context.Remove(contato);
-            _context.SaveChanges();
+                _context.Remove(contato);
+                _context.SaveChanges();
 
-            return NoContent();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception: " + ex + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    Title = "Erro",
+                    Msg = $"Erro ao atualizar registro!",
+                    Status = HttpStatusCode.InternalServerError
+                });
+            }
         }
     }
 }
